19126ab51e38961ec064a205488370d7f2f7f7b3
Antoni <49868160+antonilol@users.noreply.github.com>
2021-08-31 17:04:20 +0200

    git clone instead of go get

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 # automatic clone, compile and install for lazy users :)
 cd /tmp
-go get github.com/antonilol/ssh-chat
+rm -rf ssh-chat # git will complain otherwise
+git clone https://github.com/antonilol/ssh-chat.git
+cd ssh-chat
 make build
 sudo cp ssh-chat /bin/ssh-chat
+echo "Installed!"
