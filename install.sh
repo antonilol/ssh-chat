807310ac4b0da40fa3977c40414131a947de768f
Antoni Spaanderman <49868160+antonilol@users.noreply.github.com>
2021-08-31 18:06:57 +0200

    only say installed if no errors occurred

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ cd /tmp
 rm -rf ssh-chat # git will complain otherwise
 git clone https://github.com/antonilol/ssh-chat.git
 cd ssh-chat
-make build
-sudo cp ssh-chat /bin/ssh-chat
+make build &&
+sudo cp ssh-chat /bin/ssh-chat &&
 echo "Installed!"
