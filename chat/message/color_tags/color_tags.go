38933817d05d0d1a2579638246166e39470bf678
Antoni Spaanderman <56turtle56@gmail.com>
2021-09-01 22:33:44 +0200

    color tags (beta)

--- chat/message/color_tags/color_tags.go
+++ chat/message/color_tags/color_tags.go
@@ -5,47 +5,62 @@ import (
 )
 
 var colors = map[string]string {
+	// foreground colors
 	"black": "30",
+	"a": "30",
+
 	"red": "31",
-	"green": "32",
-	"yellow": "33",
-	"blue": "34",
-	"magenta": "35",
-	"cyan": "36",
-	"white": "37",
-
-	"bgblack": "40",
-	"bgred": "41",
-	"bggreen": "42",
-	"bgyellow": "43",
-	"bgblue": "44",
-	"bgmagenta": "45",
-	"bgcyan": "46",
-	"bgwhite": "47",
-
-	"b": "30",
 	"r": "31",
+
+	"green": "32",
 	"g": "32",
+
+	"yellow": "33",
 	"y": "33",
+
+	"blue": "34",
 	"l": "34",
+
+	"magenta": "35",
 	"m": "35",
+
+	"cyan": "36",
 	"c": "36",
+
+	"white": "37",
 	"w": "37",
 
-	"bb": "40",
+	// background colors
+	"bgblack": "40",
+	"ba": "40",
+
+	"bgred": "41",
 	"br": "41",
+
+	"bggreen": "42",
 	"bg": "42",
+
+	"bgyellow": "43",
 	"by": "43",
+
+	"bgblue": "44",
 	"bl": "44",
+
+	"bgmagenta": "45",
 	"bm": "45",
+
+	"bgcyan": "46",
 	"bc": "46",
+
+	"bgwhite": "47",
 	"bw": "47",
 
+	// special
 	"reset": "0",
 	"rs": "0",
 
 	"bold": "1",
-	"bo": "1",
+	"b": "1",
 
 	"dim": "2",
 	"d": "2",
@@ -84,26 +99,25 @@ func parseTag(i string) string {
 	return res + "m"
 }
 
-func ParseColorTags(b string) string {
+func ParseColorTags(msg string) string {
 	p := ""
-	//in_tag := false
+	in_tag := false
 	t := ""
-	for _, r := range b {
-		if r == '<' {
-			//in_tag = true
+	for _, r := range msg {
+		if r == '<' && !in_tag {
+			in_tag = true
 			p += t
 			t = ""
-		} else if r == '>' {
-			//in_tag = false
-			if t == "" {
-				//p += parseTag("rs")
-			} else {
-				p += parseTag(t)
-				t = ""
-			}
+		} else if r == '>' && in_tag {
+			in_tag = false
+			p += parseTag(t)
+			t = ""
 		} else {
 			t += string(r)
 		}
-        }
+	}
+	if in_tag {
+		p += "<"
+	}
 	return p + t + parseTag("reset")
 }
